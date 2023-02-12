4f040851bd04cb43eb2b113bcbd722ef1dc074ee
GDog <harsanyi@windowslive.com>
2023-02-12 20:50:19 +0100

    baszott lsp config

--- lua/gdog/plugins.lua
+++ lua/gdog/plugins.lua
@@ -53,10 +53,17 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" -- LSP auto complete
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LPS stuff
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/mason.nvim" -- simple to use language server installer
+  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+  use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
 
   -- Automatically set up your configuration after cloning packer.nvim
