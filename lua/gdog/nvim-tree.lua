0d5f6475a23d836a79a4a1039cba9b64d812e011
GDog <harsanyi@windowslive.com>
2023-02-14 19:40:08 +0100

    add todo

--- lua/gdog/nvim-tree.lua
+++ lua/gdog/nvim-tree.lua
@@ -1,6 +1,7 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
+--TODO: fix -> no git indicator when opened twice
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
     return
